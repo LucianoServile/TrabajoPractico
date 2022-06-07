28dcc9132b04903544d02cd857dc12ad9cfc84d4
Samuel Castro <88595777+NadalconHaste@users.noreply.github.com>
2022-06-07 18:09:15 -0300

    Update Liga.java
    Agregué algunos métodos para que herede de combatiente

--- Liga.java
+++ Liga.java
@@ -2,14 +2,16 @@ package HeroesVillanos;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.ArrayList;
 
-public class Liga {
+public class Liga extends Combatiente{
 	
 	private HashSet<Personaje> personajes;
 	private HashMap<Caracteristica, Integer> caracteristicas;
+	private ArrayList<Combatiente> combatientes = new ArrayList<Combatiente>();
 	
-	public Liga(HashSet<Personaje> personajes, HashMap<Caracteristica, Integer> caracteristicas) {
-		super();
+	public Liga(String nombreLiga,Equipo equipo,HashSet<Personaje> personajes, HashMap<Caracteristica, Integer> caracteristicas) {
+		super(nombreLiga, equipo);
 		this.personajes = personajes;
 		this.caracteristicas = caracteristicas;
 	}
@@ -36,8 +38,24 @@ public class Liga {
 		personajes.remove(personaje);
 	}
 	
-	public void enfrentarCon(Liga liga, Caracteristica caracteristica) {
+        @Override
+	public int compare(Caracteristica o1, Caracteristica o2) {
+		// creo que este comparador va en caracteristicas, en ese caso habria que elimnarla 
+		// de combatiente y las clases hijas
+		return 0;
+	}
+
+	@Override
+	public int getCaracteristica(Caracteristica c) {
 		
+		return caracteristicas.get(c);
+	}
+
+	@Override
+	public boolean esGanador(Combatiente combatiente, Caracteristica c) {
+		// este seria el metodo enfrentarCon , aca ponemos o personajes o ligas que son
+		// combatientes y el metodo devuelve si el combatiente actual es ganador o gana el retador
+		return false;
 	}
 	
 }
