5e965cb61ecbfa22321a5494d6ffd9c7a29c908f
leansw1 <102433277+leansw1@users.noreply.github.com>
2022-06-15 16:50:29 -0300

    Update Combatiente.java

--- Combatiente.java
+++ Combatiente.java
@@ -1,27 +1,27 @@
 package HeroeVillano;
 import java.util.HashMap;
-
-public abstract class Combatiente {
-
 	private String nombre; // nombre de liga o personaje
 	private Equipo equipo;
 	private HashMap<Caracteristica, Integer> caracteristicas;
 
-	public Combatiente(String nombre, Equipo equipo, int velocidad, int fuerza, int resistencia, int destreza) {
+	public Combatiente(String nombre, Equipo equipo, int velocidad, int fuerza,
+			int resistencia, int destreza) {
 		this.nombre = nombre;
 		this.equipo = equipo;
-		this.caracteristicas = new HashMap<Caracteristica,Integer>();
+		this.caracteristicas = new HashMap<Caracteristica, Integer>();
 		caracteristicas.put(Caracteristica.FUERZA, fuerza);
 		caracteristicas.put(Caracteristica.VELOCIDAD, velocidad);
 		caracteristicas.put(Caracteristica.DESTREZA, destreza);
 		caracteristicas.put(Caracteristica.RESISTENCIA, resistencia);
 	}
+
 	public boolean esGanador(Combatiente combatiente, Caracteristica c) {
-		
+
 		if (this.getEquipo().equals(combatiente.getEquipo())) {
-			System.out.println("No se pueden enfrentar 2 combatientes del mismo equipo");
+			System.out
+					.println("No se pueden enfrentar 2 combatientes del mismo equipo");
 		}
-		
+
 		boolean esGanador = false;
 		Caracteristica cAux;
 		if (this.getCaracteristica(c) == combatiente.getCaracteristica(c)) {
@@ -35,13 +35,46 @@ public abstract class Combatiente {
 
 		} else if (this.getCaracteristica(c) > combatiente.getCaracteristica(c)) {
 			esGanador = true;
-			System.out.println("El combatiente " + this.getNombre() + " es el vencedor del combate");
+			System.out.println("El combatiente " + this.getNombre()
+					+ " es el vencedor del combate");
 
 		} else if (this.getCaracteristica(c) < combatiente.getCaracteristica(c)) {
-			System.out.println("El combatiente " + combatiente.getNombre() + " es el vencedor del combate");
+			System.out.println("El combatiente " + combatiente.getNombre()
+					+ " es el vencedor del combate");
 		}
 		return esGanador;
 	}
+
+	public boolean esGanador(Liga liga, Caracteristica c) {
+
+		if (this.getEquipo().equals(liga.getEquipo())) {
+			System.out
+					.println("No se pueden enfrentar 2 ligas del mismo equipo");
+		}
+
+		boolean esGanador = false;
+		Caracteristica cAux;
+		if (this.getCaracteristica(c) == liga.getCaracteristica(c)) {
+			esGanador(liga, c.nextCaracteristica(c));
+			cAux = c.nextCaracteristica(c);
+
+			if (c.equals(cAux)) {
+				System.out.println("El resultado de la pelea es empate");
+				return esGanador;
+			}
+
+		} else if (this.getCaracteristica(c) > liga.getCaracteristica(c)) {
+			esGanador = true;
+			System.out.println("El combatiente " + this.getNombre()
+					+ " es el vencedor del combate");
+
+		} else if (this.getCaracteristica(c) < liga.getCaracteristica(c)) {
+			System.out.println("La liga: " + liga.getNombre()
+					+ " es la vencedora del combate");
+		}
+		return esGanador;
+	}
+
 	public int getCaracteristica(Caracteristica c) {
 		return caracteristicas.get(c);
 	}
@@ -49,7 +82,12 @@ public abstract class Combatiente {
 	public String getNombre() {
 		return nombre;
 	}
+
 	public Equipo getEquipo() {
 		return equipo;
+	}
+	
+	public int compareTo(Combatiente otroCombatiente, Caracteristica c) {
+		return Integer.compare(this.getCaracteristica(c),otroCombatiente.getCaracteristica(c) );
 	}
 }
