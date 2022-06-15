928daea2bf3ba6bcd8bf910e5dd6205ce7f7b686
leansw1 <102433277+leansw1@users.noreply.github.com>
2022-06-15 16:50:32 -0300

    Update Liga.java

--- Liga.java
+++ Liga.java
@@ -1,10 +1,5 @@
 package HeroesVillanos;
 
-import java.util.HashSet;
-
-public class Liga extends Combatiente{
-
-
 	private static HashSet<Combatiente> combatientes;
 	
 	public Liga(String nombre, Equipo equipo, int velocidad, int fuerza, int resistencia, int destreza, HashSet<Combatiente> combatientes) {
@@ -23,7 +18,7 @@ public class Liga extends Combatiente{
 		
 	}
 
-	//este método tiene que ser usado por otro metodo
+	//este mÃ©todo tiene que ser usado por otro metodo
 	
 	private double promediarHabilidades(Caracteristica caracteristica) {
 		double promedio = 0;
@@ -44,38 +39,15 @@ public class Liga extends Combatiente{
 		return combatientes;
 	}
 
-
-	@Override
-	public int compare(Object o1, Object o2) {
-		// TODO Auto-generated method stub
-		return 0;
+	
+	public int compareTo(Liga otraLiga, Caracteristica c) {
+		return Integer.compare(this.getCaracteristica(c),otraLiga.getCaracteristica(c) );
 	}
-//	public boolean esGanador(Combatiente combatiente, Caracteristica c) {
-//		
-//		if (this.getEquipo().equals(combatiente.getEquipo())) {
-//			System.out.println("No se pueden enfrentar 2 combatientes del mismo equipo");
-//		}
-//		
-//		boolean esGanador = false;
-//		Caracteristica cAux;
-//		if (this.getCaracteristica(c) == combatiente.getCaracteristica(c)) {
-//			esGanador(combatiente, c.nextCaracteristica(c));
-//			cAux = c.nextCaracteristica(c);
-//
-//			if (c.equals(cAux)) {
-//				System.out.println("El resultado de la pelea es empate");
-//				return esGanador;
-//			}
-//
-//		} else if (this.getCaracteristica(c) > combatiente.getCaracteristica(c)) {
-//			esGanador = true;
-//			System.out.println("El combatiente " + this.getNombre() + " es el vencedor del combate");
-//
-//		} else if (this.getCaracteristica(c) < combatiente.getCaracteristica(c)) {
-//			System.out.println("El combatiente " + combatiente.getNombre() + " es el vencedor del combate");
-//		}
-//		return esGanador;
-//	}
+	
+	public int compareTo(Combatiente otroCombatiente, Caracteristica c) {
+		return Integer.compare(this.getCaracteristica(c),otroCombatiente.getCaracteristica(c) );
+	}
+	
 	
 	public TreeSet<Integer> listarCombatienteOrdenado(Caracteristica c){
 		
