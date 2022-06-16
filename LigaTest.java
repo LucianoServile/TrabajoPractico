e9eeb233f7eb50821990c9ff6cc8cac98eea3101
LucianoServile <41164414+LucianoServile@users.noreply.github.com>
2022-06-16 14:28:15 -0300

    Update LigaTest.java

--- LigaTest.java
+++ LigaTest.java
@@ -74,16 +74,6 @@ public class LigaTest {
 		
 	}
 	
-	@Test
-	public void devuelvePromedioCorrecto() {
-		HashSet<Combatiente> combatientes = new HashSet<Combatiente>();
-		Combatiente c1 = new Villano("Batman","a", 1, 2, 7, 4);
-		Combatiente c2 = new Villano("Superman","a", 5, 3, 4, 2);
-		Combatiente c3 = new Villano("WonderWoman","a", 3, 1, 6, 3);
-		Liga liga = new Liga("Liga", Equipo.VILLANO, 0, 0, 0, 0, combatientes);
-		liga.agregarCombatiente(c1);
-		liga.agregarCombatiente(c2);
-		liga.agregarCombatiente(c3);
-	}
+
 
 }
