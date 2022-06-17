a033a48d10c3347ec469cd1ac48e761d3a48ea5d
leansw1 <102433277+leansw1@users.noreply.github.com>
2022-06-16 22:17:37 -0300

    Update Villano.java

--- Villano.java
+++ Villano.java
@@ -1,16 +1,20 @@
-package HeroesVillanos
-
 public class Villano extends Combatiente {
 	
 	private String nombreCivil;
 
-	public Villano(String nombreCivil, Equipo tipo_De_Personaje, String nombre, int velocidad, int fuerza, int resistencia, int destreza) {
-		super(nombre, tipo_De_Personaje, velocidad, fuerza, resistencia, destreza);
+	public Villano(String nombreCivil, String nombre, int velocidad, int fuerza, int resistencia, int destreza) {
+		super(nombre, Equipo.VILLANO, velocidad, fuerza, resistencia, destreza);
 		this.nombreCivil = nombreCivil;
 	}
 	
+	// este metodo devuelve true si el combatiente actual le gana al combatiente ingresado de acuerdo a la 
+	// caracteristica solicitada, si empatan definen x la siguiente caracteristica.
+	//falta agregar q un villano solo se pueda enfrentar a un heroe/liga de heroes.
+	
+
 	public String getNombreCivil() {
 		return nombreCivil;
 	}
 
 }
+
