5e208eb8a3d27f92642fd1348f473bf9e42ba1b8
leansw1 <102433277+leansw1@users.noreply.github.com>
2022-06-16 22:40:48 -0300

    Update Heroe.java

--- Heroe.java
+++ Heroe.java
@@ -1,15 +1,19 @@
-package heroesVillanos
+
 public class Heroe extends Combatiente {
 
 	private String nombreCivil;
 
-	public Heroe(String nombreCivil, Equipo tipo_De_Personaje, String nombre, int velocidad, int fuerza, int resistencia, int destreza) {
-		super(nombre, tipo_De_Personaje, velocidad, fuerza, resistencia, destreza);
+	public Heroe(String nombreCivil, String nombre, int velocidad, int fuerza, int resistencia, int destreza) {
+		super(nombre, Equipo.HEROE, velocidad, fuerza, resistencia, destreza);
 		this.nombreCivil = nombreCivil;
 	}
+	
+	// este metodo devuelve true si el heroe actual le gana al combatiente ingresado de acuerdo a la 
+	// caracteristica solicitada, si empatan definen x la siguiente caracteristica.
+	//falta agregar q un heroe solo se pueda enfrentar a un villano/liga de villanos.
 
 	public String getNombreCivil() {
 		return nombreCivil;
 	}
-
+	
 }
