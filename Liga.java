20eb961ca679401f32165c6781e2c2cb348d6dca
leansw1 <102433277+leansw1@users.noreply.github.com>
2022-06-26 22:21:04 -0300

    Update Liga.java

--- Liga.java
+++ Liga.java
@@ -4,61 +4,60 @@ import java.util.HashSet;
 import java.util.TreeSet;
 
 
-public class Liga extends Combatiente{
-	
-	private static HashSet<Combatiente> combatientes;
-	
-	public Liga(String nombre, Equipo equipo, int velocidad, int fuerza, int resistencia, int destreza, HashSet<Combatiente> combatientes) {
+public class Liga extends Combatiente {
+
+	private HashSet<Combatiente> combatientes;
+
+	public Liga(String nombre, Equipo equipo, int velocidad, int fuerza,
+			int resistencia, int destreza, HashSet<Combatiente> combatientes) {
 		super(nombre, equipo, velocidad, fuerza, resistencia, destreza);
-		Liga.combatientes = combatientes;
 		this.combatientes = new HashSet<Combatiente>();
 	}
 
-	
 	public void agregarCombatiente(Combatiente combatiente) {
-		
-		if(this.getEquipo() == combatiente.getEquipo()) {
+
+		if (this.getEquipo() == combatiente.getEquipo()) {
 			combatientes.add(combatiente);
 		} else {
-			System.err.println(combatiente.getNombre() + " No es del mismo equipo");
+			System.err.println(combatiente.getNombre()
+					+ " No es del mismo equipo");
 		}
-		
+
 	}
 
-	//este mÃ©todo tiene que ser usado por otro metodo
-	
+	// este mÃƒÂ©todo tiene que ser usado por otro metodo
+
 	public double promediarHabilidades(Caracteristica caracteristica) {
 		double promedio = 0;
 		int cantidad = 0;
-		for (Combatiente c: combatientes) {
+		for (Combatiente c : combatientes) {
 			promedio += c.getCaracteristica(caracteristica);
 			cantidad++;
 		}
-		return promedio/cantidad;
-		
+		return promedio / cantidad;
+
 	}
-	
+
 	public void eliminarCombatiente(Combatiente combatiente) {
 		combatientes.remove(combatiente);
 	}
 
-	public static HashSet<Combatiente> getCombatientes() {
+	public HashSet<Combatiente> getCombatientes() {
 		return combatientes;
 	}
-	
-	
-	public TreeSet<Integer> listarCombatienteOrdenado(Caracteristica c){
-		
+
+	public TreeSet<Integer> listarCombatienteOrdenado(Caracteristica c) {
+
 		TreeSet<Integer> listaOrdenada = new TreeSet<Integer>();
-		
-		for(Combatiente comb : combatientes) {
+
+		for (Combatiente comb : combatientes) {
 			listaOrdenada.add(comb.getCaracteristica(c));
 		}
-		
+
 		return listaOrdenada;
 	}
-	
-@Override
+
+	@Override
 	public String toString() {
 		Iterator<Combatiente> itr1 = combatientes.iterator();
 		String datos = " "+ "Velocidad: " + super.getCaracteristica(Caracteristica.VELOCIDAD) + ", "
